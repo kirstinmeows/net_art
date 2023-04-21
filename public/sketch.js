e73ab43d28941cfe9958754b5fac669eed495d5b
Kirstin Marshall <kirstinmarshall@Kirstins-MacBook-Pro.local>
2023-04-21 18:19:55 +1000

    resize canvas without using p5

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,34 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
+const cnv = document.getElementById ('canvas01')
+cnv.style.background = ""
+
+const ctx = cnv.getContext ("2d")
+
+resize_canvas ()
+
+window.onresize = resize_canvas
+
+let x_pos = 0 
+
+requestAnimationFrame (draw_frame)
+
+
 function setup () {
    createCanvas (innerWidth, innerHeight)
 }
 
-function  draw () {
-   background (`turquoise`)
-   square (width/2, height/2, 100)
-   fill (`deeppink`)
+function  draw_frame () 
+{
+ctx.fillStyle = "turqoise"
+ctx.fillRect (0, 0, cnv.width, cnv.height)
+
+requestAnimationFrame (draw_frame)
+}
+
+function resize_canvas ()
+{
+   cnv.width = innerWidth 
+   cnv.height = innerHeight
 }
